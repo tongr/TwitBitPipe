c297b81d3675d3648faa87cd7db9b50c5f2573c1
tongr <tongr@online>
2013-02-27 11:52:34 +0100

    simple run script added

---
+++ run.js
@@ -0,0 +1,12 @@
+﻿var 
+  twitterCredentials = require('./twitter.credentials.private'),
+  rabbitSettings = require('./rabbitmq.config.private'),
+  Pipe = require('./lib/pipe');
+
+var pipeInstance = new Pipe(twitterCredentials, rabbitSettings);
+
+// filter all tweets containing links and pipe it to the linkTweets exchange
+pipeInstance.filter(
+  'statuses/filter', 
+  { 'track' : ['http'] }, 
+  'linkTweets');
