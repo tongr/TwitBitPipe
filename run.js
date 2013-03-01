9303a8d349c50c90a6051966bc877ed525868c9c
tongr <tongr@online>
2013-03-01 16:38:59 +0100

    typo removed

--- run.js
+++ run.js
@@ -13,25 +13,28 @@ var fileOut = new FileSink(fileConfig);
 pipeInstance.add(fileOut);
 
 
+/*
+pipeInstance.init('statuses/filter', { 'track' : ['http'] });
+/*/
 //add rabbit sink
 var rabbitOut = new RabbitSink(rabbitSettings);
 pipeInstance.add(rabbitOut);
 
-//pipeInstance.init('statuses/filter', { 'track' : ['http'] });
-
 // init the rabbit connection ..
 // after connection to rabbit was established, start twitter streaming api access
 rabbitOut.init({
-  name : 'tweets',
-  type : 'fanout',
-  durable : false,
-  exclusive : false,
-  autoDelete : true,
-  content : {
-    contentEncoding : 'utf8', 
-    contentType : 'application/json'
+    name : 'tweets',
+    type : 'fanout',
+    durable : false,
+    exclusive : false,
+    autoDelete : true,
+    content : {
+      contentEncoding : 'utf8', 
+      contentType : 'application/json'
+	}
   }, function() {
     // filter all tweets containing links and pipe it to the previously defined file sinks
     pipeInstance.init('statuses/filter', { 'track' : ['http'] });
   }
 );
+//*/
