915b9b6ff62e13d5cf8255f176ffff9c7c9395b6
tongr <tongr@online>
2013-07-23 14:40:13 +0200

    log file sizes reduced after (re-)start

--- scripts/startForever.sh
+++ scripts/startForever.sh
@@ -5,6 +5,12 @@ module_dir=$(dirname $0)/..
 
 cd $module_dir
 
+# trim old log files
+mv ./forever.log ./forever.log.backup
+tail -n 100 ./forever.log.backup > ./forever.log
+mv ./forever.error ./forever.error.backup
+tail -n 100 ./forever.error.backup > ./forever.error
+
 node ./runForever.js >> ./forever.log 2>> ./forever.error &
 echo $!
 echo $! > ./forever.pid
